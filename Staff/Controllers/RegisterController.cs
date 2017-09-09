11a2282a2114d282dfb90be9bae1b803ac0efdfa
Gabriel Barrett <gbarrett2@gmail.com>
2017-09-09 07:31:30 -0600

    complete the intial build of register entity - fields, controller, and views

---
+++ Staff/Controllers/RegisterController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Staff.Models;
+
+namespace Staff.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private StaffDB db = new StaffDB();
+
+        // GET: Register
+        public ActionResult Index()
+        {
+            var registers = db.Registers.Include(r => r.Person);
+            return View(registers.ToList());
+        }
+
+        // GET: Register/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Registers.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            return View(register);
+        }
+
+        // GET: Register/Create
+        public ActionResult Create()
+        {
+            ViewBag.PersonId = new SelectList(db.People, "PersonId", "FirstName");
+            return View();
+        }
+
+        // POST: Register/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RegisterId,PersonId,Cash,Check,Visa,Mastercard,Discover,Amex,GiftCard,Tax,Date")] Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Registers.Add(register);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PersonId = new SelectList(db.People, "PersonId", "FirstName", register.PersonId);
+            return View(register);
+        }
+
+        // GET: Register/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Registers.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersonId = new SelectList(db.People, "PersonId", "FirstName", register.PersonId);
+            return View(register);
+        }
+
+        // POST: Register/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RegisterId,PersonId,Cash,Check,Visa,Mastercard,Discover,Amex,GiftCard,Tax,Date")] Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(register).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PersonId = new SelectList(db.People, "PersonId", "FirstName", register.PersonId);
+            return View(register);
+        }
+
+        // GET: Register/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Registers.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            return View(register);
+        }
+
+        // POST: Register/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Register register = db.Registers.Find(id);
+            db.Registers.Remove(register);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Staff/Models/Register.cs
+++ Staff/Models/Register.cs
@@ -10,8 +10,26 @@ namespace Staff.Models
     {
         public int RegisterId { get; set; }
 
+        public int PersonId { get; set; }
+
         public decimal Cash { get; set; }
 
+        public decimal Check { get; set; }
 
+        public decimal Visa { get; set; }
+
+        public decimal Mastercard { get; set; }
+
+        public decimal Discover { get; set; }
+
+        public decimal Amex { get; set; }
+
+        public decimal GiftCard { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public virtual Person Person { get; set; }
     }
 }
